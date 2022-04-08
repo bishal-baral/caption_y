62b9c9be9f65ad67512977d7a17bdd7d608de429
bishal-baral <bishalbaral@brandeis.edu>
2022-04-07 22:40:08 -0400

    setting up react with flask

---
+++ backend/base.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+api = Flask(__name__)
+
+@api.route('/message')
+def my_profile():
+    response_body = {
+        "message": "Hello World!",
+    }
+
+    return response_body
