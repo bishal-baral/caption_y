759742da00fb055ead2e1e16b05f570d95aeba4a
ayuksekkaya <alisyuksekkaya@gmail.com>
2022-05-04 21:52:07 -0400

    Build ESindex for testing

---
+++ backend/es_service/test.py
@@ -0,0 +1,29 @@
+from elasticsearch_dsl import Search
+from elasticsearch_dsl.connections import connections
+from elasticsearch_dsl.query import Match, MatchAll, ScriptScore, Ids, Query
+
+
+def search(index: str, query: Query) -> None:
+    s = Search(using="default", index=index).query(query)[
+        :30
+    ]  # initialize a query and return top five results
+    response = s.execute()
+    for hit in response:
+        print(
+            hit.meta.score, hit.title, sep="\t"
+        )  # print the document id that is assigned by ES index, score and title
+
+
+if __name__ == "__main__":
+
+    connections.create_connection(
+        hosts=["localhost"], timeout=100, alias="default")
+
+    query_text = " Rory yells at Paris and Tristin."
+    q_basic = Match(
+        content={"query": query_text}
+    )
+
+    search(
+        "movies", q_basic
+    )
