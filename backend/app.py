ab409a4761136c4fbb9e33e6fc7aa1ddc6356933
bishal-baral <bishalbaral@brandeis.edu>
2022-05-06 18:44:06 -0400

    render results from backend

--- backend/app.py
+++ backend/app.py
@@ -1,18 +1,27 @@
 from flask import Flask, request
+import json
 
 api = Flask(__name__)
 
-@api.route('/message')
-def my_profile():
-    response_body = {
-        "message": "Hello World!",
-    }
-    return response_body
 
 @api.route('/results', methods=['GET', 'POST'])
 def get_results():
     print(request.data)
-    response_body = {
-        "results" : request.get_json()["query"]
-    }
-    return response_body
+    response_body = [
+            {
+                "title": f"This is the 1st title for query: {request.get_json()['query']}",
+                "imdb_url": "https://www.google.com/",
+                "description": "This is a description"
+            },
+            {
+                "title": f"This is the 2nd title for query: {request.get_json()['query']}",
+                "imdb_url": "https://www.google.com/",
+                "description": "This is the second description"
+            },
+            {
+                "title": f"This is the 3rd title for query: {request.get_json()['query']}",
+                "imdb_url": "https://www.google.com/",
+                "description": "This is the last description"
+            }
+    ]
+    return json.dumps(response_body)
--- src/App.js
+++ src/App.js
@@ -1,72 +1,56 @@
 import { useState } from 'react'
 import axios from "axios";
-import logo from './logo.svg';
 import './App.css';
-import SearchBar from './components/SearchBar';
-
-// function App() {
-
-//   const [messages, setMessages] = useState(null)
-
-//   function getData() {
-//     axios({
-//       method: "GET",
-//       url:"/message",
-//     })
-//     .then((response) => {
-//       const res =response.data
-//       console.log(res);
-//       setMessages(({
-//         message: res.message}))
-//     }).catch((error) => {
-//       if (error.response) {
-//         console.log(error.response)
-//         console.log(error.response.status)
-//         console.log(error.response.headers)
-//         }
-//     })}
-
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-        
-//         <p>To get your profile details: </p><button onClick={getData}>Click me</button>
-//         {
-//           messages && 
-//           <div>
-//             <p>Message: {messages.message}</p>
-//           </div>
-//         }
-//       </header>
-//     </div>
-//   );
-// }
-
-// const App = () => (
-//   <SearchBar />
-// );
 
 function App(){
-  const [search, setSearch] = userState("")
+  const [search, setSearch] = useState("")
   const [results, setResults] = useState([]);
-  const [searchInfo, setSearchInfo] = useState([]);
+
+  const handleSearch = async e => {
+    e.preventDefault();
+    if (search === '') return;
+    const response = await axios({
+      method: "POST",
+      url:"/results",
+      data: {
+        query: search,
+      }
+    })
+    console.log(response.data);
+    setResults(response.data)    
+  }
 
   return (
+    
     <div className="App">
       <header>
         <h1> Captiony </h1>
-        <form className="search-box">
-          <input type="search" placeholder="What are you looking for?"/>
+        <form 
+          className="search-box"
+          onSubmit={handleSearch}
+        >
+          <input 
+            type="search" 
+            placeholder="What are you looking for?"
+            value={search}
+            onChange={e => setSearch(e.target.value)}
+          />
         </form>
           <p>Search Results: 0</p>
       </header>
 
       <div className="results">
-        <div className="result">
-          <h3> Title goes here</h3>
-          <p> lorem ipsum dolor sit amet, consectetur lorem ipsum dolor sit amet lorem ipsum dolor sit am</p>
-          <a href="#"> Read More </a>
-        </div>
+        {
+          results.map((result, i) =>{
+            return (
+              <div key={"result"+i} className="result">
+                <h3>{result.title}</h3>
+                <p> {result.description}</p>
+                <a href={result.imdb_url} target="_blank" rel="nofollow"> View on IMDB </a>
+              </div>
+            )
+          })
+        }
       </div>
     </div>
   );
