dc96e59d3a5c0b1e0120d438ec65fd9f1c1abc99
bishal-baral <bishalbaral@brandeis.edu>
2022-05-06 22:23:08 -0400

    display search results from es_index

--- backend/es_service/test.py
+++ backend/es_service/test.py
@@ -8,22 +8,36 @@ def search(index: str, query: Query) -> None:
         :30
     ]  # initialize a query and return top five results
     response = s.execute()
+    result = []
     for hit in response:
-        print(
-            hit.meta.score, hit.title, sep="\t"
-        )  # print the document id that is assigned by ES index, score and title
+        print(hit)
+        result.append(
+            {
+                "title": hit.title, 
+                # "imdb_url": hit.imdb_url
+            })
+        # print(
+        #     hit.meta.score, hit.title, sep="\t"
+        # )  # print the document id that is assigned by ES index, score and title
+    return result
 
-
-if __name__ == "__main__":
-
-    connections.create_connection(
-        hosts=["localhost"], timeout=100, alias="default")
-
-    query_text = " Rory yells at Paris and Tristin."
+def make_query(query_text):
+    connections.create_connection(hosts=["localhost"], timeout=100, alias="default")
     q_basic = Match(
         content={"query": query_text}
     )
+    return search("movies", q_basic)
+    
+if __name__ == "__main__":
+    pass
+    # connections.create_connection(
+    #     hosts=["localhost"], timeout=100, alias="default")
+    
+    # query_text = "Rory yells at Paris and Tristin."
+    # q_basic = Match(
+    #     content={"query": query_text}
+    # )
 
-    search(
-        "movies", q_basic
-    )
+    # search(
+    #     "movies", q_basic
+    # )
