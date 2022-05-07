31e989cbb7ad9d5cbcddede9e4bef0d10f934459
ayuksekkaya <alisyuksekkaya@gmail.com>
2022-05-07 18:17:19 -0400

    renamed test to search

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 import json
+from es_service.search import make_query
 
 api = Flask(__name__)
 
@@ -7,21 +8,5 @@ api = Flask(__name__)
 @api.route('/results', methods=['GET', 'POST'])
 def get_results():
     print(request.data)
-    response_body = [
-            {
-                "title": f"This is the 1st title for query: {request.get_json()['query']}",
-                "imdb_url": "https://www.google.com/",
-                "description": "This is a description"
-            },
-            {
-                "title": f"This is the 2nd title for query: {request.get_json()['query']}",
-                "imdb_url": "https://www.google.com/",
-                "description": "This is the second description"
-            },
-            {
-                "title": f"This is the 3rd title for query: {request.get_json()['query']}",
-                "imdb_url": "https://www.google.com/",
-                "description": "This is the last description"
-            }
-    ]
+    response_body = make_query(request.get_json()['query'])
     return json.dumps(response_body)
