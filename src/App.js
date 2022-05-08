e9814fe6aaeb60e17a89f3c6f437927215af5acc
bishal-baral <bishalbaral@brandeis.edu>
2022-05-08 00:48:56 -0400

    added radio buttons to select content_type

--- src/App.js
+++ src/App.js
@@ -5,23 +5,46 @@ import './App.css';
 function App(){
   const [search, setSearch] = useState("")
   const [results, setResults] = useState([]);
+  const [isPlot, setIsPlot] = useState(true);
+  const [isCaption, setIsCaption] = useState(false);
 
   const handleSearch = async e => {
     e.preventDefault();
     if (search === '') return;
+
+    let content_type = ""
+    if (isPlot){
+      content_type = "plot"
+    }
+    else if (isCaption){
+      content_type = "caption"
+    }
+
     const response = await axios({
       method: "POST",
       url:"/results",
       data: {
         query: search,
+        content_type: content_type,
       }
     })
     console.log(response.data);
     setResults(response.data)    
   }
 
+  const handleIsPlot = () => {
+    console.log("option1 selected");
+    setIsPlot(!isPlot);
+    setIsCaption(false);
+  };
+
+  const handleIsCaption = () => {
+    console.log("option2 selected");
+    setIsCaption(!isCaption);
+    setIsPlot(false);
+  };
+
   return (
-    
     <div className="App">
       <header>
         <h1> Caption-Y </h1>
@@ -29,7 +52,18 @@ function App(){
           className="search-box"
           onSubmit={handleSearch}
         >
+        <div className="search-box-radio">
+          <label>
+            <input type="radio" checked={isPlot} onChange={handleIsPlot} />
+            Plot
+          </label>
+          <label>
+            <input type="radio" checked={isCaption} onChange={handleIsCaption} />
+            Caption
+          </label>
+        </div>
           <input 
+            className="search-box-bar"
             type="search" 
             placeholder="What are you looking for?"
             value={search}
@@ -52,7 +86,6 @@ function App(){
                   <p>This is the description</p>
                   <a href={result.imdb_url} target="_blank" rel="nofollow"> View on IMDB </a>
                   {/* <p> {result.description}</p> */}
-                  {/* <a href={result.imdb_url} target="_blank" rel="nofollow"> View on IMDB </a> */}
                 </div>
                 
               </div>
