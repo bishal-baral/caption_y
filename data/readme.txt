78fc477eb1e890a7ddbcb7d00d84c9fd0aa8acc8
ayuksekkaya <alisyuksekkaya@gmail.com>
2022-05-08 02:35:59 -0400

    new data link, add try catch

--- data/readme.txt
+++ data/readme.txt
@@ -1,3 +1,3 @@
 To download the data go to this link:
 
-https://drive.google.com/file/d/1vMj-oI8Vh2X61pEafqK8cBOhgFGNh33i/view?usp=sharing
+https://drive.google.com/file/d/1THYKwfoK9TZxkxtXV6QcfrMqtRNaOHOu/view?usp=sharing
--- utils/omdbdata.py
+++ utils/omdbdata.py
@@ -8,30 +8,32 @@ pp = PrettyPrinter()
 
 
 def getData(id: str):
-    #Fetch Movie Data with Full Plot 
+    # Fetch Movie Data with Full Plot
     api_key = "INSERTHERE"
     title = id
     data_URL = 'http://www.omdbapi.com/?i='+title+'&apikey='+api_key
     # print(data_URL)
-    year = ''
     params = {
-        'type':'movie',
-        'y':year,
-        'plot':'full',
+        'plot': 'full'
     }
-    response = requests.get(data_URL,params=params).json()
+    response = requests.get(data_URL, params=params).json()
     # pp.pprint(response)
     return response
 
+
 def create_index():
-    with open("../data/test_captions.jl", encoding='utf-8') as f:
+    with open("../data/captions.jl", encoding='utf-8') as f:
         for line in f:
             movie_data = json.loads(line)
             imdb_id = movie_data["imdb_id"]
-            more_movie_data = getData(imdb_id)
-            more_movie_data["content"] = movie_data["content"]
+            try:
+                more_movie_data = getData(imdb_id)
+                more_movie_data["content"] = movie_data["content"]
 
-            with open("../data/captions_updated.jl", 'a', encoding='utf-8') as f2:
-                f2.write(json.dumps(more_movie_data) + "\n")
+                with open("../data/captions_updated.jl", 'a', encoding='utf-8') as f2:
+                    f2.write(json.dumps(more_movie_data) + "\n")
+            except:
+                print("skipping", imdb_id)
 
-create_index()    
+
+create_index()
